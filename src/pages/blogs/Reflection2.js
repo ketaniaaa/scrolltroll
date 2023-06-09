8e0b25f60ea31a8b9d5c7d93092756d2020623fc
Ketania Govender <keethe1975@icloud.com>
2023-06-09 20:48:19 +0200

    style guide refs

--- src/pages/blogs/Reflection2.js
+++ src/pages/blogs/Reflection2.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Reflection2 extends Component {
   render() {
     return (
-      <div>Reflection2</div>
+      <div>Reflection2 let us seen</div>
     )
   }
 }
