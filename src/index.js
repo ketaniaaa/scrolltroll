f706c19f5f6bca1eb393e9e6d72b3042aad7f689
Ketania Govender <keethe1975@icloud.com>
2023-06-09 11:59:04 +0200

    Hash Router to fix 404 errors

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  
+    <Router>
     <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    </Router>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
