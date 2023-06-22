e6d0094095b5b00bef35f2a2603182f4cf26c261
Ketania Govender <keethe1975@icloud.com>
2023-06-22 23:50:42 +0200

    resolving compile errors

--- src/pages/forms/password.js
+++ src/pages/forms/password.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ProgressBar from "../../images/formimages/progressbar3.svg";
 export default function Password() {
 
